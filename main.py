22649d6f1b429085c452f3e8ede6102697cabe9a
Kevin Läufer <laeufer@cs.berkeley.edu>
2018-10-16 15:15:47 -0700

    try to write inverter

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from firrtl import *
+from typing import Tuple
 
+def out(name: str, ty: Type) -> Port:
+	return Port(name=name, ty=ty, dir=PortDir.Output)
 
-def ports(**kwargs):
-	return [Port(name=name, ty=ty) for name, ty in kwargs]
+def inp(name: str, ty: Type) -> Port:
+	return Port(name=name, ty=ty, dir=PortDir.Input)
 
-def module(name, ports, statements):
+def ports(*args: Port) -> List[Port]:
+	return list(args)
+
+def module(name: str, ports: List[Port], statements: List[Statement]):
 	return Module(name=name, ports=ports, statements=statements)
 
 
 def make_circuit():
-	return Circuit(name="Gcd", modules=[
-		module("Gcd",
-			ports(
-				clock=Clock(),
-				reset=UInt(1),
-
-
-			   ),
+	return Circuit(name="Inv", modules=[
+		module("Inv",
+			ports(inp('clk', Clock()), inp('reset', UInt(1)), inp('in', UInt(1)), out('out', UInt(1))),
 			   []
 			   )
 	])
