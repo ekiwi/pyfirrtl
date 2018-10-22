b273a66500ebb27736471052307192361156a524
Kevin Laeufer <ekiwi@berkeley.edu>
2018-10-22 12:42:21 -0700

    wip

---
+++ deep_thought.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, University of California, Berkeley
+# author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+
+from aga import *
+
+
+
+
+def main():
+	print("Hello World")
+
+	# elaborate
+
+	# simulate for N cycles
+
+
+
+"""
+module mkTestbench (Empty);
+rule rl_print_answer;
+$display ("Deep Thought says: Hello, World! The answer is 42.");
+$finish;
+endrule
+endmodule
+"""
+
+if __name__ == '__main__':
+	main()
