b346c8e79707da4805bfdcc7c35c79187d3ab254
Kevin Läufer <laeufer@cs.berkeley.edu>
2018-10-23 15:45:39 -0700

    [gcd] try to rewrite in new syntax

--- gcd.py
+++ gcd.py
@@ -7,6 +7,23 @@
 from aga import *
 
 
+class Gcd(Module):
+	def __init__(self, typ: Type):
+		super().__init__(
+			x = RegU(typ),
+			y = Reg(typ, 0)
+		)
+		with self.rule("swap").if(x > y and y != 0) as r:
+			r.update(x=y, y=x)
+		with self.rule("subtract").if(x <= y and y != 0) as r:
+			r.update(y=y-x)
+		with self.method(Action, "start", a=typ, b=typ).if(y == 0) as m:
+			m.update(x=a, y=b)
+		with self.method(typ, "result").if(y == 0) as m:
+			m.ret(x)
+
+
+
 def mkGcd(typ):
 	x = mkRegU('x', typ)
 	y = mkReg('y', typ, 0)
