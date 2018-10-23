5abbfaa18ca4365f46b778dd097495bdc6e2e37f
Kevin Läufer <laeufer@cs.berkeley.edu>
2018-10-23 16:06:54 -0700

    multi module example

--- deep_thought.py
+++ deep_thought.py
@@ -7,16 +7,40 @@
 from aga import *
 
 
+###############################################################################
+
+"""
 class Testbench(Module):
 	def __init__(self):
 		super().__init__()
 		with self.rule("rl_print_answer") as r:
 			r.display("Deep Thought says: Hello, World! The answer is 42.")
 			r.finish()
+"""
 
+###############################################################################
+
+class DeepThought(Module):
+	def __init__(self, T):
+		super().__init__()
+		with self.value(T, "getAnswer") as r:
+			r.ret(T(42))
+
+class Testbench(Module):
+	def __init__(self, T):
+		super().__init__()
+		self.deepThought = DeepThought(T)
+		with self.rule("rl_print_answer") as r:
+			x = self.deepThought.getAnswer()
+			r.display("Deep Thought says: Hello, World! The answer is %d.", x)
+			r.finish()
+
+
+###############################################################################
 
 def main():
-	circuit = elaborate(Testbench())
+	T = UInt(32)
+	circuit = elaborate(Testbench(T))
 	simulate(circuit, max_cycles=10)
 
 
